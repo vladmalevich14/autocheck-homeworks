8e4d4b940c69e6df3810f4803bc0dfc87dd62260
vladmalevich14 <vladmalevich1431@gmail.com>
2022-10-18 23:07:40 +0300

    third commit

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -55,6 +55,7 @@ function HW2() {
     return (
         <div id={'hw2'}>
             <div className={s2.hwTitle}>Homework #2</div>
+            <hr/>
             <div className={s2.hw}>
                 <Affairs
                     data={filteredAffairs}
@@ -63,6 +64,7 @@ function HW2() {
                     filter={filter}
                 />
             </div>
+            <hr/>
         </div>
     )
 }
