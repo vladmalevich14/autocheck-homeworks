e8a3bd1a122d22c60081bff56fc9a06dc9587b69
vladmalevich14 <vladmalevich1431@gmail.com>
2022-10-18 23:35:56 +0300

    fourth commit

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -42,15 +42,11 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
     const [error, setError] = useState<string>('') // need to fix any
 
     const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
-        const inputValue = e.currentTarget.value.trim()
-        if (inputValue) {
-            setName(inputValue)
-            error && setError('')
-        } else {
-            name && setName('')
-            setError('Пожалуйста введите имя')
-        } // need to fix
+        setName(e.currentTarget.value) // need to fix
+
+        error && setError('')
     }
+
     const addUser = () => {
         pureAddUser(name, setError, setName, addUserCallback)
     }
@@ -64,7 +60,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
     }
 
     const totalUsers = users.length // need to fix
-    const lastUserName = users.length > 0 ? users[users.length-1].name : '' // need to fix
+    const lastUserName = users.length !==0 ? users[users.length-1].name : '' // need to fix
 
     return (
         <>
