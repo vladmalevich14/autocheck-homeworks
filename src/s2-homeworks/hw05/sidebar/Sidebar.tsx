d33768af2e9013a3c0099463353b55d244950d57
vladmalevich14 <vladmalevich1431@gmail.com>
2022-10-20 22:09:45 +0300

    seventh commit

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -24,14 +24,15 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         alt="close sidebar"
                         id={'hw5-menu-close'}
                     />
-                </button>
+                </button>w
 
                 <nav id={'hw5-menu'} className={s.nav}>
                     <NavLink
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({isActive}) =>
+                            isActive ? s.active : ''}// делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +40,8 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({isActive}) =>
+                            isActive ? s.active : ''} // делает студент
                     >
                         Junior
                     </NavLink>
@@ -47,7 +49,8 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({isActive}) =>
+                            isActive ? s.active : ''} // делает студент
                     >
                         Junior Plus
                     </NavLink>
