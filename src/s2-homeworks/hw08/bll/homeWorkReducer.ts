f9906a26b3dfc80c484dbaf4cc5848dfb9d30405
vladmalevich14 <vladmalevich1431@gmail.com>
2022-10-22 19:36:15 +0300

    eleventh commit

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,12 +7,13 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            const stateCopy = [...state]
             if(action.payload === 'up') {
-                return state.sort((a, b) => a.name > b.name ? 1 : -1)
+                return stateCopy.sort((a, b) => a.name > b.name ? 1 : -1)
             } else if(action.payload === 'down') {
-                state.sort((a, b) => a.name < b.name ? 1 : -1) // need to fix
+                stateCopy.sort((a, b) => a.name < b.name ? 1 : -1) // need to fix
         }
-            return state
+            return stateCopy
         }
         case 'check': {
             return state.filter(u=>u.age > 18) // need to fix
