00419acd73d12ae61e0aee81aa81051b1cb05f92
vladmalevich14 <vladmalevich1431@gmail.com>
2022-12-19 22:54:27 +0300

    loader_commit

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,26 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <div className={s.loader}>
+    <svg className="whole" width="100" height="100" viewBox="0 0 100 100" fill="none"
+         xmlns="http://www.w3.org/2000/svg">
+        <g id="loader">
+            <animateTransform
+                xlinkHref="#loader"
+                attributeName="transform"
+                attributeType="XML"
+                type="rotate"
+                from="0 50 50"
+                to="360 50 50"
+                dur="1s"
+                begin="0s"
+                repeatCount="indefinite"
+                restart="always"
+            />
+            <path className="a" opacity="0.2" fill-rule="evenodd" clip-rule="evenodd"
+                  d="M50 100C77.6142 100 100 77.6142 100 50C100 22.3858 77.6142 0 50 0C22.3858 0 0 22.3858 0 50C0 77.6142 22.3858 100 50 100ZM50 90C72.0914 90 90 72.0914 90 50C90 27.9086 72.0914 10 50 10C27.9086 10 10 27.9086 10 50C10 72.0914 27.9086 90 50 90Z"
+                  fill="#66B1DC"/>
+            <path className="b" fill-rule="evenodd" clip-rule="evenodd"
+                  d="M100 50C100 22.3858 77.6142 0 50 0V10C72.0914 10 90 27.9086 90 50H100Z" fill="#66B1DC"/>
+        </g>
+    </svg>
+</div>
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,10 +2,15 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+type InitStateType = {
+    isLoading: boolean
+}
+
+export const loadingReducer = (state:InitStateType = initState, action: LoadingActionType): InitStateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING':
+            return {...state, isLoading: action.isLoading}
         default:
             return state
     }
